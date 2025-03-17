d8ef806ca5c4e4ea4fcb5fb0e293aa2c57eab6b2
Ryan Schmidt <skizzerz@skizzerz.net>
2025-03-17 14:09:48 -0700

    WIP tls-exporter support

---
+++ Netwolf.Transport/Internal/ExporterChannelBinding.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net.Security;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netwolf.Transport.Internal;
+
+internal class ExporterChannelBinding : ChannelBinding
+{
+    private int Length { get; set; }
+
+    public override int Size => Length;
+
+    internal unsafe static ExporterChannelBinding? Build(SslStream stream)
+    {
+        /* References: https://github.com/dotnet/runtime/tree/v9.0.3/src/libraries/System.Net.Security/src/System/Net/Security
+         * SslStream.*.cs, SslStreamPal.*.cs
+         */
+        if (typeof(SslStream)?.GetField("_securityContext", BindingFlags.NonPublic)?.GetValue(stream) is not SafeHandle context)
+        {
+            return null;
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            // Values per RFC 9266
+            NativeInteropSSPI.SecPkgContext_KeyingMaterialInfo info = new()
+            {
+                cbLabel = 25, // length of "EXPORTER-Channel-Binding", including terminating NULL byte
+                pszLabel = "EXPORTER-Channel-Binding",
+                cbContextValue = 0, // no context is passed
+                pbContextValue = nint.Zero,
+                cbKeyingMaterial = 32 // we need 32 bytes back out
+            };
+
+            var result = NativeInteropSSPI.SetContextAttributes(
+                context,
+                NativeInteropSSPI.SECPKG_ATTR_KEYING_MATERIAL_INFO,
+                info,
+                (uint)Marshal.SizeOf<NativeInteropSSPI.SecPkgContext_KeyingMaterialInfo>());
+
+            NativeInteropSSPI.SecPkgContext_KeyingMaterial binding = new();
+
+            result = NativeInteropSSPI.QueryContextAttributes(
+                context,
+                NativeInteropSSPI.SECPKG_ATTR_KEYING_MATERIAL,
+                ref binding);
+        }
+    }
+
+    private ExporterChannelBinding(nint handleValue, int length)
+    {
+        handle = handleValue;
+        Length = length;
+    }
+
+    protected override bool ReleaseHandle()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Netwolf.Transport/Internal/NativeInteropSSPI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+// compiler complains that arrays as attribute values aren't CLS compliant despite the class being internal
+#pragma warning disable CS3016
+
+namespace Netwolf.Transport.Internal
+{
+    internal static partial class NativeInteropSSPI
+    {
+        internal const int SEC_E_OK = 0;
+        internal const int SECPKG_ATTR_KEYING_MATERIAL_INFO = 0x6a;
+        internal const int SECPKG_ATTR_KEYING_MATERIAL = 0x6b;
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct SecPkgContext_KeyingMaterial
+        {
+            internal uint cbKeyingMaterial;
+            internal nint pbKeyingMaterial;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        internal struct SecPkgContext_KeyingMaterialInfo
+        {
+            internal ushort cbLabel;
+            internal string pszLabel;
+            internal ushort cbContextValue;
+            internal nint pbContextValue;
+            internal uint cbKeyingMaterial;
+        }
+
+        [LibraryImport("Secur32", EntryPoint = "QueryContextAttributes")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvStdcall)])]
+        internal static partial int QueryContextAttributes(SafeHandle phContext, uint ulAttribute, ref SecPkgContext_KeyingMaterial pBuffer);
+
+        [LibraryImport("Secur32", EntryPoint = "SetContextAttributesW")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvStdcall)])]
+        internal static partial int SetContextAttributes(SafeHandle phContext, uint ulAttribute, SecPkgContext_KeyingMaterialInfo pBuffer, uint cbBuffer);
+
+        [LibraryImport("Secur32", EntryPoint = "FreeContextBuffer")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvStdcall)])]
+        internal static partial int FreeContextBuffer(nint pvContextBuffer);
+    }
+}
